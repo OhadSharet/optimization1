f45728c7851527b16207e19ef84aa74239a21366
ohads <ohad.sharet@gmcil.com>
2021-03-29 15:58:38 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+def ex1b():
+    pass
+
+
+def gram_schmidt(A):
+    n = len(A)
+    R = np.zeros(np.zeros((n,n)))
+
+def print_hi(name):
+    val = 3
+    print(val ** 7)
+
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    test = np.array([[3.0, 1.0], [2.0, 2.0]])
+    test2 = np.array([[1.0, 1.0, 0.0], [1.0, 3.0, 1.0], [2.0, -1.0, 1.0]])
+
+    print(np.array(gs(test)))
+    print(np.array(gs(test2)))
+    print_hi('PyCharm')

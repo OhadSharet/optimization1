19221e611b0fe8c3058597b896d563d75ac69085
ohads <ohad.sharet@gmcil.com>
2021-03-29 23:09:08 +0300

    ex1b code added

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import numpy as np
-
+import math
 
 def ex1b():
-    pass
+    A = np.array([[1,2,3,4],
+               [2,4,-4,8],
+               [-5,4,1,5],
+               [5,0,-3,-7]])
+    AT = A.transpose()
+    ATA = AT  @ A
+    eigen_vals,w = np.linalg.eig(ATA)
+
+    print("eigen_vals : \n%s "%str(eigen_vals))
+    print ("ATA : \n %s"%str(ATA))
+    max_sigma = math.sqrt(max(eigen_vals))
+    print ( "max sigma : %s"%max_sigma)
+    x = w@np.array([[1],[0],[0],[0]])
+    print ("x : %s"%str(x))
+
+
 
 
 def gram_schmidt(A):
@@ -10,16 +25,10 @@ def gram_schmidt(A):
     R = np.zeros(np.zeros((n,n)))
 
 def print_hi(name):
-    val = 3
-    print(val ** 7)
+    ex1b()
 
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 if __name__ == '__main__':
-    test = np.array([[3.0, 1.0], [2.0, 2.0]])
-    test2 = np.array([[1.0, 1.0, 0.0], [1.0, 3.0, 1.0], [2.0, -1.0, 1.0]])
-
-    print(np.array(gs(test)))
-    print(np.array(gs(test2)))
     print_hi('PyCharm')
